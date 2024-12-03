7350f7fc4ae9b0397d5452dce2e4a3834ebd695a
naumanalin <naumanalin865@gmail.com>
2024-12-03 16:14:45 +0500

    remove testing changing

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,3 @@ const server = http.createServer(app)
 server.listen(port, ()=>{
     console.log(`server is running on port ${port}`)
 })
-const http = require('http')
-const app = require('./app')
-const port = process.env.PORT || 8001
-
-const server = http.createServer(app)
-
-server.listen(port, ()=>{
-    console.log(`server is running on port ${port}`)
-})
